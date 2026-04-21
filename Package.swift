47cb7142b4383b044b9e6ff2e0b4762d80e6affb
fw1088 <fw1088@163.com>
2026-04-21 16:58:35 -0700

    chore: add Package.swift to root

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MySDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "MySDK",
+            targets: ["MySDK"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MySDK",
+            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.0/MySDK.xcframework.zip",
+            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+        )
+    ]
+)
