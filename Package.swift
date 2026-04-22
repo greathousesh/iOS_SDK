adf19226aab3c69da54bf9e8f593cefbb9acafa2
fw1088 <fw1088@163.com>
2026-04-21 17:12:43 -0700

    chore: fixed Package.swift for v0.0.13

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MySDK",
-            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.12/MySDK.xcframework.zip",
+            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.13/MySDK.xcframework.zip",
             checksum: "ae446a765b707e4e966b8c52c6d01b91d2f7b6c53c6d0bff78600d142d298a39"
         )
     ]
