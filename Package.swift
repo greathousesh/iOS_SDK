c075acc57386a28d06308b2b031a460654eb7e4b
fw1088 <fw1088@163.com>
2026-04-21 17:40:33 -0700

    fix: update Package.swift for v0.0.23 with correct checksum

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MySDK",
-            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.22/MySDK.xcframework.zip",
+            url: "https://github.com/greathousesh/iOS_SDK/releases/download/v0.0.23/MySDK.xcframework.zip",
             checksum: "7a1eafe6ddaea6ec7c815ab171fa56dd53c593ddebdf3c176296fb40c6b76028"
         )
     ]
